e285c3f071e01e2c6a795f68b5c7279994565c82
Pallavi Karan <pallavi.karan@teladochealth.com>
2025-04-18 19:08:49 -0500

    adding basics

---
+++ Python-Problems/01 Arrays/P15ReverseAnArrayOrString.py
@@ -0,0 +1,37 @@
+"""
+Write a function that reverses a string. The input string is given as an array of characters s.
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+Constraints:
+1 <= s.length <= 105
+s[i] is a printable ascii character.
+"""
+
+def reverse_string(s):
+    """
+    :type s: List[str]
+    :rtype: None Do not return anything, modify s in-place instead.
+    """
+    left_indx = 0
+    right_indx = len(s) - 1
+
+    while left_indx < right_indx:
+        s[left_indx], s[right_indx] = s[right_indx], s[left_indx]
+        left_indx += 1
+        right_indx -= 1
+
+    return s
+
+s = ["h","e","l","l","o"]
+print(reverse_string(s))
+
+s = ["H","a","n","n","a","h"]
+print(reverse_string(s))
