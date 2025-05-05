d22d00e0690c64a3e9a312dcf9069b231373e500
pallavikaran <karanpallavi@gmail.com>
2025-05-05 17:06:49 -0500

    Merge SetZeros 2D matrix

--- Python-Problems/01 Arrays/02Matrix/P49RotateImage.py
+++ Python-Problems/01 Arrays/02Matrix/P49RotateImage.py
@@ -18,5 +18,104 @@ Constraints:
 n == matrix.length == matrix[i].length
 1 <= n <= 20
 -1000 <= matrix[i][j] <= 1000
+"""
 
 """
+1 2 3      1 4 7        7 4 1
+4 5 6  =>  2 5 8   =>   8 5 2
+7 8 9      3 6 9        9 6 3
+Original   Transposed   Reversed
+"""
+
+
+def rotate_90(matrix):
+    """
+    :type matrix: List[List[int]]
+    :rtype: None Do not return anything, modify matrix in-place instead.
+    """
+    # 90 degree -> Transpose(row -> col, col-> rows) then Reverse matrix
+    # 180 degree -> Reverse matrix twice
+    # 360 degree -> Return original matrix
+
+    # Transpose Matrix
+    for row_itr in range(0, len(matrix)):
+        for col_itr in range(row_itr + 1, len(matrix)):
+            matrix[row_itr][col_itr], matrix[col_itr][row_itr] = matrix[col_itr][row_itr], matrix[row_itr][col_itr]
+
+    # Reverse the matrix
+    for row in matrix:
+        row.reverse()
+
+    return matrix
+
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(rotate_90(matrix))
+
+matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+print(rotate_90(matrix))
+
+
+
+"""
+1 2 3       3 2 1
+4 5 6  =>   6 5 4
+7 8 9       9 8 9
+Original    Reversed
+"""
+
+
+def rotate_180(matrix):
+    """
+    :type matrix: List[List[int]]
+    :rtype: None Do not return anything, modify matrix in-place instead.
+    """
+    # 90 degree -> Transpose(row -> col, col-> rows) then Reverse matrix
+    # 180 degree -> Reverse matrix twice
+    # 360 degree -> Return original matrix
+
+    # Transpose Matrix
+    for row_itr in range(0, len(matrix)):
+        for col_itr in range(row_itr + 1, len(matrix)):
+            matrix[row_itr][col_itr], matrix[col_itr][row_itr] = matrix[col_itr][row_itr], matrix[row_itr][col_itr]
+
+    # Reverse the matrix
+    for row in matrix:
+        row.reverse()
+
+    return matrix
+
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(rotate_180(matrix))
+
+matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+print(rotate_180(matrix))
+
+
+def rotate_360(matrix):
+    """
+    :type matrix: List[List[int]]
+    :rtype: None Do not return anything, modify matrix in-place instead.
+    """
+    # 90 degree -> Transpose(row -> col, col-> rows) then Reverse matrix
+    # 180 degree -> Reverse matrix twice
+    # 360 degree -> Return original matrix
+
+    # Transpose Matrix
+    for row_itr in range(0, len(matrix)):
+        for col_itr in range(row_itr + 1, len(matrix)):
+            matrix[row_itr][col_itr], matrix[col_itr][row_itr] = matrix[col_itr][row_itr], matrix[row_itr][col_itr]
+
+    # Reverse the matrix
+    for row in matrix:
+        row.reverse()
+
+    return matrix
+
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(rotate_360(matrix))
+
+matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+print(rotate_360(matrix))
--- Python-Problems/01 Arrays/02Matrix/P50SearchA2DMatrix.py
+++ Python-Problems/01 Arrays/02Matrix/P50SearchA2DMatrix.py
@@ -22,5 +22,45 @@ m == matrix.length
 n == matrix[i].length
 1 <= m, n <= 100
 -104 <= matrix[i][j], target <= 104
+"""
 
 """
+Binary Search Strategy:
+Treat the matrix as a virtual 1D array of length m * n.
+For a given index k in the 1D view:
+1. Row = k // n
+2. Column = k % n
+3. Apply binary search using these conversions.
+"""
+
+
+def search_matrix(matrix, target):
+    """
+    :type matrix: List[List[int]]
+    :type target: int
+    :rtype: bool
+    """
+    if not matrix or not matrix[0]:
+        return False
+
+    m, n = len(matrix), len(matrix[0])
+    left, right = 0, m * n - 1 # Since indices are 0-based, the last index in this flattened array is m * n - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        row, col = divmod(mid, n) # divmod(a, b) returns: (a // b, a % b) ex: row, col = divmod(6, 4)  # returns (1, 2)
+        # mid // n gives the row index, mid % n gives the column index.
+        mid_val = matrix[row][col]
+
+        if mid_val == target:
+            return True
+        elif mid_val < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return False
+
+
+print(search_matrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
+print(search_matrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 17))
